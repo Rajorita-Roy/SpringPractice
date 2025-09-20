3ea52c111b3b247528cce0d01fc4ba63196c6c27
Rajorita-Roy <rajorita.roy@cognizant.com>
2025-09-20 17:41:43 +0530

    Update ProductService.java

--- Problem-2/src/main/java/com/example/ProductService.java
+++ Problem-2/src/main/java/com/example/ProductService.java
@@ -25,40 +25,33 @@ public class ProductService {
 
     public List<Product> getProductsSortedByName() {
         List<Product> sortedList = new ArrayList<>(products.values());
-        // Sorts using the natural order defined in Product (by name)
         sortedList.sort(null);
         return sortedList;
     }
 
     public Set<Product> getProductsSortedByPrice() {
-        // Use a TreeSet with a custom comparator to sort by price
         Set<Product> sortedSet = new TreeSet<>(Product.sortByPrice());
         sortedSet.addAll(products.values());
         return sortedSet;
     }
 
     public static void main(String[] args) {
-        // Load the Spring context from applicationContext.xml
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-        // Get the ProductService bean
         ProductService productService = (ProductService) context.getBean("ProductService");
-
-        // Add products to the service
+        
         productService.addProduct(new Product(101, "Laptop", 1200.50));
         productService.addProduct(new Product(102, "Mouse", 25.00));
         productService.addProduct(new Product(103, "Keyboard", 75.75));
         productService.addProduct(new Product(104, "Monitor", 350.25));
 
-        // Display products sorted by name
         System.out.println("Products sorted by name:");
         List<Product> sortedByName = productService.getProductsSortedByName();
         sortedByName.forEach(System.out::println);
         System.out.println();
 
-        // Display products sorted by price
         System.out.println("Products sorted by price:");
         Set<Product> sortedByPrice = productService.getProductsSortedByPrice();
         sortedByPrice.forEach(System.out::println);
     }
 }
+
